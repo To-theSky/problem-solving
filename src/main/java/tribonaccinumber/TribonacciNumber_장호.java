292e5d446ee3ee658762880479ecbaa4fac0f52e
pjhsk113 <pjhsk113@naver.com>
2020-12-24 19:37:29 +0900

    [feat] Tribonacci Solved

--- src/main/java/tribonaccinumber/TribonacciNumber_장호.java
+++ src/main/java/tribonaccinumber/TribonacciNumber_장호.java
@@ -1,12 +1,8 @@
 package tribonaccinumber;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class TribonacciNumber {
+public class TribonacciNumber_장호 {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
@@ -18,12 +14,26 @@ public class TribonacciNumber {
 
         input = 25; output = 1389537;
         assertThat(solution.tribonacci(input)).isEqualTo(output);
+
+        input = 0; output = 0;
+        assertThat(solution.tribonacci(input)).isEqualTo(output);
     }
 
     static class Solution {
-
         public int tribonacci(int n) {
+            if (n == 0) return 0;
+            if (n == 1 || n == 2) return 0;
 
+            int[] memo = new int[n+1];
+
+            memo[0] = 0;
+            memo[1] = 1;
+            memo[2] = 1;
+
+            for (int i = 3; i <= n; i++ ) {
+                memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
+            }
+            return memo[n];
         }
     }
 }
